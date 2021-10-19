c353cd1071401082e44fec29a15af8dd1ada9c04
Sivanesh S <86925605+narc-kany@users.noreply.github.com>
2021-10-19 22:23:01 +0530

    Add files via upload
    Added the project file (string interpolation, lists, linkedlist, variable types)

---
+++ GradeBook/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=3.142, y=22.001; 
+            var ans=0.00;
+            ans = y/x;
+            double result = 0.00;
+
+            double[] num = new double[] {13.51, 6.43, 8.25, 90.0};
+            foreach(double n in num) {
+                result += n;
+                System.Console.WriteLine($"The sum is {result}");
+            }
+            Console.WriteLine($"the value of the double {ans} \n");
+            if (args.Length == 0) 
+            {
+                Console.WriteLine("No value is inside");
+            }
+            else 
+            {
+             Console.WriteLine($"Welcome {args[0]} !\n" + getSalutations(args[0]) + "\n");        
+             
+            }
+
+            //List 
+            List<double> grades = new List<double>() {13.51, 6.43, 8.25, 90.0};
+            grades.Add(90.99);
+
+            foreach(double grade in grades) {
+                result += grade;
+            }
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average range is {result:N2}");
+
+
+
+            
+        }
+
+        public static String getSalutations(string username)
+        {
+            return "Sup" + username;
+        }
+    }
+}
